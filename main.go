cadd15a2053f61711cdfb5e3dc2f68687bdc18f4
Allexsen <asvanidze12@gmail.com>
2024-06-17 12:15:13 +0200

    Basic backend functionality
    Gin router and linking set up. In-memory "database" for adding and retrieval simulation.

--- main.go
+++ main.go
@@ -1,53 +1,65 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+	"strconv"
 
-	"golang.org/x/tour/tree"
+	"github.com/gin-gonic/gin"
 )
 
-// Walk walks the tree t sending all values
-// from the tree to the channel ch.
-func Walk(t *tree.Tree, ch chan int) {
-	var walk func(t *tree.Tree)
-	walk = func(t *tree.Tree) {
-		if t == nil {
-			return
-		}
-		walk(t.Left)
-		ch <- t.Value
-		walk(t.Right)
-	}
-
-	walk(t)
-	close(ch)
-}
-
-// Same determines whether the trees
-// t1 and t2 contain the same values.
-func Same(t1, t2 *tree.Tree) bool {
-	ch1, ch2 := make(chan int), make(chan int)
-
-	go Walk(t1, ch1)
-	go Walk(t2, ch2)
-
-	for {
-		select {
-		case v1, ok1 := <-ch1:
-			v2, ok2 := <-ch2
-			if !ok1 || !ok2 {
-				return ok1 == ok2
-			}
-			if v1 != v2 {
-				return false
-			}
-		}
-	}
+type Record struct {
+	// Will get an UID later on
+	Name          string
+	Email         string
+	HoursWorked   int
+	MinutesWorked int
 }
 
 func main() {
-	t1, t2 := tree.New(1), tree.New(2)
-	fmt.Println(Same(t1, t1))
-	fmt.Println(Same(t1, t2))
-	fmt.Println(Same(t2, t2))
+	r := gin.Default()
+
+	r.Static("/static", "./")
+
+	r.GET("/", func(c *gin.Context) {
+		c.File("./index.html")
+	})
+
+	var Records []Record
+	r.POST("/records/add", func(c *gin.Context) {
+		// Temporary In-Memory "database"
+		name := c.PostForm("name")
+		email := c.PostForm("email")
+		hStr := c.PostForm("hours")
+		minStr := c.PostForm("minutes")
+
+		hours, err := strconv.Atoi(hStr)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error:": "Invalid hours format"})
+			return
+		}
+
+		minutes, err := strconv.Atoi(minStr)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error:": "Invalid minutes format"})
+			return
+		}
+
+		record := Record{name, email, hours, minutes}
+		Records = append(Records, record)
+	})
+
+	r.POST("/records/retrieve", func(c *gin.Context) {
+		email := c.PostForm("email")
+
+		for _, record := range Records {
+			if record.Email == email {
+				c.JSON(http.StatusOK, record)
+				return
+			}
+		}
+
+		c.JSON(http.StatusNotFound, gin.H{"error": "Record not found"})
+	})
+
+	r.Run()
 }
