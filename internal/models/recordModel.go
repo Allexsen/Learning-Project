12c2b902953a05a609176fe9d060a136abb1031c
Allexsen <asvanidze12@gmail.com>
2024-06-23 21:13:17 +0200

    Models
    Created "User" and "Record" model, and their basic functionality of db interaction.

---
+++ internal/models/recordModel.go
@@ -0,0 +1,21 @@
+package models
+
+import (
+	"github.com/Allexsen/Learning-Project/internal/db"
+)
+
+type Record struct {
+	ID      string
+	UserID  string
+	Hours   int
+	Minutes int
+}
+
+func (r *Record) AddRecord() (int64, error) {
+	result, err := db.DB.Exec("INSERT INTO practiceDB.records (user_id, hours, minutes) VALUES (?, ?, ?)", r.UserID, r.Hours, r.Minutes)
+	if err != nil {
+		return 0, err
+	}
+
+	return result.LastInsertId()
+}
--- internal/models/userModel.go
+++ internal/models/userModel.go
@@ -1,1 +1,29 @@
 package models
+
+import "github.com/Allexsen/Learning-Project/internal/db"
+
+type User struct {
+	ID           string
+	Name         string
+	Email        string
+	TotalHours   int
+	TotalMinutes int
+	LogCount     int
+	Records      []Record
+}
+
+func (u *User) AddUser() (int64, error) {
+	result, err := db.DB.Exec(`INSERT INTO practiceDB.users (name, email) VALUES (?, ?)`, u.Name, u.Email)
+	if err != nil {
+		return 0, err
+	}
+
+	return result.LastInsertId()
+}
+
+func (u *User) RetrieveUser() error {
+	err := db.DB.QueryRow(`SELECT * FROM practiceDB.users WHERE id=?`, u.ID).Scan(
+		&u.ID, &u.Name, &u.Email, &u.TotalHours, &u.TotalMinutes, &u.LogCount)
+
+	return err
+}
