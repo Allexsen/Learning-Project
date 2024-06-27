105c094fec82022b8f4c88b4bde46d3b05b0c5d0
Allexsen <asvanidze12@gmail.com>
2024-06-27 11:57:17 +0200

    Code Cleanup
    Extra piece of old code was leftover, removed.

--- internal/router/recordsRouter.go
+++ internal/router/recordsRouter.go
@@ -4,14 +4,6 @@ import (
 	"github.com/Allexsen/Learning-Project/internal/handlers"
 )
 
-type Record struct {
-	// Will get an UID later on
-	Name          string
-	Email         string
-	HoursWorked   int
-	MinutesWorked int
-}
-
 func initRecordsRouter() {
 	r.POST("/record/add", handlers.RecordAdd())
 }
