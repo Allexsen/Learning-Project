bbc7ade7e05cda00e397f864695e47ee44b4dd04
Allexsen <asvanidze12@gmail.com>
2024-07-05 00:34:55 +0200

    JWT utils
    utils for generating and validating JWT. Might need refactoring/redistributing later, or changing other settings. Acts as a potentially temporary placeholder for now.

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -1,1 +1,53 @@
 package utils
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+var jwtKey = []byte(os.Getenv("JWT_SECRET"))
+
+type Claims struct {
+	Username string `json:"username"`
+	jwt.StandardClaims
+}
+
+func GenerateJWT(username string) (string, error) {
+	expirationTime := time.Now().Add(24 * time.Hour)
+	claims := &Claims{
+		Username: username,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(jwtKey)
+	if err != nil {
+		return "", fmt.Errorf("couldn't get the token string: %v", err)
+	}
+
+	return tokenString, nil
+}
+
+func ValidateJWT(tokenString string) (*Claims, error) {
+	claims := &Claims{}
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		return jwtKey, nil
+	})
+
+	if err != nil {
+		if err == jwt.ErrSignatureInvalid {
+			return nil, fmt.Errorf("invalid signature: %v", err)
+		}
+		return nil, fmt.Errorf("couldn't parse the token: %v", err)
+	}
+	if !token.Valid {
+		return nil, fmt.Errorf("invalid token: %v", err)
+	}
+
+	return claims, nil
+}
