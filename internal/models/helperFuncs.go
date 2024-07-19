34c9cbc474af3a9993ecaefe81185a0567e770f9
Allexsen <asvanidze12@gmail.com>
2024-07-19 18:40:13 +0200

    improved error types & context
    changed error context containing "error": err field as it's already logged by logger. The error message was being lost, changed to provide it as context details instead, like everywhere else in the code.
    
    Now also returns better error types made accessible by new error type creation.

--- internal/models/helperFuncs.go
+++ internal/models/helperFuncs.go
@@ -20,12 +20,34 @@ func getLastInsertId(result sql.Result, q string, data interface{}) (int64, erro
 		return -1, apperrors.New(
 			http.StatusInternalServerError,
 			"Couldn't retrieve last insert ID",
-			err,
-			map[string]interface{}{"query": q, dataType: data},
+			apperrors.ErrDBLastInsertId,
+			map[string]interface{}{"query": q, dataType: data, "error": err},
 		)
 	}
 
 	return lastInsertId, nil
+}
+
+// getQueryError checks if there was an error in execution.
+// Returns AppError if found any, or nil otherwise.
+func getQueryError(q, message string, data interface{}, err error) error {
+	if err == nil {
+		return nil
+	}
+
+	code, errType := http.StatusInternalServerError, apperrors.ErrDBQuery
+	if err == sql.ErrNoRows { // no rows == resource was not found
+		code = http.StatusNotFound
+		errType = apperrors.ErrDBNoRows
+	}
+
+	dataType := reflect.TypeOf(data).Name()
+	return apperrors.New(
+		code,
+		message,
+		errType,
+		map[string]interface{}{"query": q, dataType: data, "details": err.Error()},
+	)
 }
 
 // handleUpdateQuery validates sql update query by checking
@@ -37,7 +59,7 @@ func handleUpdateQuery(result sql.Result, err error, q string, data interface{})
 			http.StatusInternalServerError,
 			fmt.Sprintf("Couldn't alter the %s info", dataType),
 			apperrors.ErrDBQuery,
-			map[string]interface{}{"query": q, dataType: data, "error": err},
+			map[string]interface{}{"query": q, dataType: data, "details": err.Error()},
 		)
 	}
 
@@ -46,13 +68,12 @@ func handleUpdateQuery(result sql.Result, err error, q string, data interface{})
 		return apperrors.New(
 			http.StatusInternalServerError,
 			"Couldn't retrieve rows affected",
-			apperrors.ErrDBQuery,
-			map[string]interface{}{"query": q, dataType: data, "error": err},
+			apperrors.ErrDBRowsAffected,
+			map[string]interface{}{"query": q, dataType: data, "details": err.Error()},
 		)
 	}
 
-	// If no rows were affected, then the requested resource was not found
-	if rowsAffected == 0 {
+	if rowsAffected == 0 { // no rows affected == resource was not found
 		return apperrors.New(
 			http.StatusNotFound,
 			fmt.Sprintf("No %s found with the given ID", dataType),
@@ -62,27 +83,4 @@ func handleUpdateQuery(result sql.Result, err error, q string, data interface{})
 	}
 
 	return nil
-}
-
-// getQueryError checks if there was an error in execution.
-// Returns AppError if found any, or nil otherwise.
-func getQueryError(q, message string, data interface{}, err error) error {
-	if err == nil {
-		return nil
-	}
-
-	code, errType := http.StatusInternalServerError, apperrors.ErrDBQuery
-	// If the resource was not found, return 400 and ErrNotFound.
-	if err == sql.ErrNoRows {
-		code = http.StatusBadRequest
-		errType = apperrors.ErrNotFound
-	}
-
-	dataType := reflect.TypeOf(data).Name()
-	return apperrors.New(
-		code,
-		message,
-		errType,
-		map[string]interface{}{"query": q, dataType: data, "details": err.Error()},
-	)
 }
